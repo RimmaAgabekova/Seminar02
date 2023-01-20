411baa7dddf02a1b913c9536d702229230918317
Rimma <mudalieva@yandex.ru>
2023-01-20 22:53:57 +0400

    внесены изменения

--- work03/Program.cs
+++ work03/Program.cs
@@ -6,11 +6,11 @@
 Console.WriteLine("Введите число от 1 до 7: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num == 6 && num == 7)
+if(num == 6 && num == 7)
 {
-Console.Write(num + "-> Да");
+    Console.Write(num + "-> Да");
 }
 else
 {
-Console.Write(num + " -> Нет");
+    Console.Write(num + " -> Нет");
 }
